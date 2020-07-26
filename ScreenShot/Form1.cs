1a07f9b608a6550f407620b4f8c704425ad253ee
dev.v.nazarenko@gmail.com <dev.v.nazarenko@gmail.com>
2020-07-26 21:55:30 +0300

    Add project files.

---
+++ ScreenShot/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Awesomium.Core;
+
+namespace ScreenShot
+{
+    public partial class Form1 : Form
+    {
+        class WebsiteCaptureMaker
+        {
+            private WebBrowser internetBrowser;
+
+            public WebsiteCaptureMaker() //браузер
+            {
+                internetBrowser = new WebBrowser(); //экземпляр браузера
+                internetBrowser.ScrollBarsEnabled = false;
+                internetBrowser.ScriptErrorsSuppressed = true;
+                
+            }
+
+            public Bitmap MakeScreenshot(string _websiteURL) //метод скриншота
+            {
+                internetBrowser.Navigate(_websiteURL);//заходим на сайт, не отображая скроллбара но показывая скрипты
+                while (internetBrowser.ReadyState != WebBrowserReadyState.Complete || internetBrowser.IsBusy)
+                Application.DoEvents();
+
+                internetBrowser.Width = internetBrowser.Document.Body.ScrollRectangle.Width; //ширина скрина
+                internetBrowser.Height = internetBrowser.Document.Body.ScrollRectangle.Height; //высота скрина
+
+                Bitmap websiteScreenshot = new Bitmap(internetBrowser.Width, internetBrowser.Height);
+                internetBrowser.DrawToBitmap(websiteScreenshot, new Rectangle(0, 0, internetBrowser.Width, internetBrowser.Height));
+
+                return websiteScreenshot;
+            }
+
+            public void Dispose()
+            {
+                internetBrowser.Dispose();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int h = 0;
+            string date = DateTime.Now.ToString("yyyyMMddhhmm"); //дата скрина
+            WebsiteCaptureMaker captureMaker = new WebsiteCaptureMaker();
+            Bitmap websiteCapture = captureMaker.MakeScreenshot(textBox1.Text);
+            websiteCapture.Save("screenshot" + date + ".jpg");
+            //websiteCapture.Dispose();
+            websiteCapture.Dispose();
+        }
+    }
+}
---
+++ ScreenShot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Awesomium.Core;
+using FlashBrokerLib;
+
+namespace ScreenShot
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+
+
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            /*var webView = WebCore.CreateWebView(1920, 1080);
+            webView.Source = new Uri("http://videoprobki.ua/camera/183-ulitsa-bratislavskaya-lesnoi-prospekt?c=Kyiiv");
+
+            webView.LoadingFrameComplete += (s, e) =>
+            {
+                if (!e.IsMainFrame)
+                    return;
+                BitmapSurface surface = (BitmapSurface)webView.Surface;
+                surface.SaveToPNG("result.png", true);
+
+                WebCore.Shutdown();
+            };
+
+            WebCore.Run();
+            webView.Dispose();*/
+        }
+    }
+}
